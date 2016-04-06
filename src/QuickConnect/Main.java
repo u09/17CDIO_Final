d23b509653387019655048b3d6466254d98e7e62
Ahmad <ahmad.almajdi@yahoo.dk>
2016-04-06 09:21:33 +0200

    x

--- src/QuickConnect/Main.java
+++ src/QuickConnect/Main.java
@@ -9,11 +9,5 @@ import QuickConnect_GUI.startFrame;
 public class Main {
 	public static void main(String[] args) throws FontFormatException, IOException, SQLException {
 		startFrame.start();
-		Connector con=Function.mysql();
-		ResultSet rs=con.select("SELECT NOW()");
-		rs.next();
-		System.out.println(rs.getString("NOW()"));
-		con.getConnection().close();
-
 	}
 }
