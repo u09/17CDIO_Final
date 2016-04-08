ebe79cd0695941e6cc8ace55571c85ab7af07f13
samilesma <samilesma@gmail.com>
2016-04-08 21:16:13 +0200

    delete user og change Password

--- src/QuickConnect/FunctionUser.java
+++ src/QuickConnect/FunctionUser.java
@@ -9,10 +9,32 @@ import java.sql.SQLException;
 
 public class FunctionUser {
 	
+	static Connector con = Function.mysql();
+	
 	public static void ChangeNickname(String nickname, String username) throws SQLException{
-		Connector con = Function.mysql();
 		con.update("UPDATE nickname FROM users=? WHERE username=?", nickname,username);
-		
+	}
+	public static void changePassword(String username, String oldPass, String newPass, String newPass2) throws SQLException, NoSuchAlgorithmException{
+		if(Function.checkPassword(newPass)==0 && newPass.equals(newPass2)){
+			con.update("update users set password=? where username =?", Function.md5(newPass),username);
+		}else if(newPass.equals(newPass2)){
+			// password stemmer ikke
+		}else if(Function.checkPassword(newPass)==1){
+			// Password skal være 8-24
+		}else if(Function.checkPassword(newPass)==2){
+			// Password skal indeholde et tal, stort og et lille bogstav
+		}else {
+			// Passwordet må kun indeholde tegn fra ascii<32 || ascii>126
+		}
+	}
+	public static void deleteUser(String username, String password) throws SQLException, NoSuchAlgorithmException{
+		boolean bool = false;
+			bool = con.check("SELECT username FROM users WHERE username=? AND password=?", username, Function.md5(password));
+		if(bool){
+			con.update("DELETE FROM users WHERE username=?",username);
+		}else{
+			System.out.println("Password forkert");
+		}
 	}
 
 }
