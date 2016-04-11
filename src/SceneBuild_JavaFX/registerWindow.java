938bea32500793975c073fda847bab8b4751413f
samilesma <samilesma@gmail.com>
2016-04-11 18:39:46 +0200

    RegisterFrame med SceneBuilder

--- src/SceneBuild_JavaFX/registerWindow.java
+++ src/SceneBuild_JavaFX/registerWindow.java
@@ -36,9 +36,9 @@ public class registerWindow extends Application implements EventHandler<ActionEv
 	@FXML
 	Button bRegister, bBack;
 	@FXML
-	TextField inUser;
+	TextField inUser, inMail;
 	@FXML
-	PasswordField inPass;
+	PasswordField inPass, inNewPass;
 
 	@Override
 	public void start(Stage stage) {
@@ -76,12 +76,110 @@ public class registerWindow extends Application implements EventHandler<ActionEv
 		
 //		bLogin.setOnAction(this);
 		bRegister.setOnAction(this);
+		bBack.setOnAction(this);
 		lTitle.getStyleClass().add("titles");
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
-		
+		// handle for bBack
+		if(event.getSource() == bBack) {
+			Stage stage = new Stage();
+			loginWindow sF = new loginWindow();
+			try {
+				sF.start(stage);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			((Node) (event.getSource())).getScene().getWindow().hide();
+		}
+		// handle for bRegister
+		if(event.getSource() == bRegister) {
+
+			String cuser = inUser.getText();
+			String cpass1 = inPass.getText();
+			String cpass2 = inNewPass.getText();
+			String cemail = inMail.getText();
+
+			int check = 0;
+			try {
+				check = checkRegister(cuser, cpass1, cpass2, cemail);
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			String msg;
+			if(check == 0) msg = "Uforventet fejl";
+			else if(check == 1) msg = "Brugernavnet skal være 4-24 tegn";
+			else if(check == 2) msg = "Brugernavnet må kun indeholde tal, bogstaver og følgende tegn: -,_";
+			else if(check == 3) msg = "Begge passwords skal være ens";
+			else if(check == 4) msg = "Passwordet skal være 8-24 tegn";
+			else if(check == 5) msg = "Passwordet skal mindst indeholde 1 stort bogstav, 1 lille bogstav og 1 tal";
+			else if(check == 6) msg = "Passwordet må kun indeholde tal, bogstaver og følgende tegn:<br><h2>!\"#$%&'(,)*+-./:;<=>?@[\\]^_`{|}~</h2>";
+			else if(check == 7) msg = "Ugyldigt email";
+			else if(check == 8) msg = "Brugeren eksisterer allerede i systemet";
+			else msg = "Du er registreret";
+
+			if(check == 9) {
+				try {
+					boolean bool = addUser(cuser, cpass1, cemail);
+				} catch(SQLException | NoSuchAlgorithmException e) {
+					e.printStackTrace();
+				}
+
+				Stage stage = new Stage();
+				loginWindow Sf = new loginWindow();
+				try {
+					Sf.start(stage);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+				((Node) (event.getSource())).getScene().getWindow().hide();
+			} else {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Forkert kode");
+				alert.setHeaderText("Forkert kodeord eller brugernavn");
+				// alert.setContentText("<html>Registrering
+				// mislykkedes!<br><br>"+msg+"</html>");
+
+				inPass.setText("");
+				inNewPass.setText("");
+				inUser.requestFocus();
+			}
+		}
+	}
+
+	public boolean addUser(String user, String pass, String email) throws SQLException, NoSuchAlgorithmException {
+		Connector con = Function.mysql();
+		con.update("INSERT INTO users (username,password,email,timestamp) VALUES (?,?,?,?)", new String[] { "s", user },
+		        new String[] { "s", Function.md5(pass) }, new String[] { "s", email },
+		        new String[] { "l", Long.toString(Function.timestamp()) });
+		return false;
+	}
+
+	public int checkRegister(String user, String pass1, String pass2, String email) throws SQLException {
+		Connector con = Function.mysql();
+		boolean bool;
+		int in;
+		in = Function.checkUsername(user);
+		if(in == 1)
+			return 1;
+		else if(in == 2)
+			return 2;
+		if(!pass1.equals(pass2))
+			return 3;
+		in = Function.checkPassword(pass1);
+		if(in == 1)
+			return 4;
+		else if(in == 2)
+			return 5;
+		else if(in == 3)
+			return 6;
+		if(!Function.checkEmail(email))
+			return 7;
+		bool = con.check("SELECT username FROM users WHERE username=?", user);
+		if(bool)
+			return 8;
+		else return 9;
 	}
 
 }
